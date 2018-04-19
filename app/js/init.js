32573c6f19955949559e6e3cec388d1a1d262e03
dmgame <denis.m.pcspace@gmail.com>
2018-04-19 07:13:13 +0300

    lesson 16

--- app/js/init.js
+++ app/js/init.js
@@ -3,7 +3,23 @@ $('select').styler();
 
 // Slick carousel init
 $('.trending-carousel').slick({
+
+    arrows:false,
+    dots: true,
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1
+});
+
+// Switch product items view
+$('.grid-thumbs').on('click', function(){
+    // Find cards and remove class row-card
+    $('.all-items-container .card').removeClass('row-card');
+    $('.all-items-container .col-4').removeClass('col-12');
+});
+
+$('.grid-list').on('click', function(){
+    // Find cards and add class row-card
+    $('.all-items-container .card').addClass('row-card');
+    $('.all-items-container .col-4').addClass('col-12');
 });
